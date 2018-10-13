f3f87cd9781afd7f1116788aba5b91d740a87150
Brayden Robbins <braydenrobbins98@gmail.com>
2018-10-13 09:38:05 -0500

    Working JS code

---
+++ example1.js
@@ -0,0 +1,11 @@
+var message = "Hello World: this is statement";
+
+console.log(message + " " + 1);
+console.log(message + " " + 2);
+console.log(message + " " + 3);
+console.log(message + " " + 4);
+console.log(message + " " + 5);
+console.log(message + " " + 6);
+console.log(message + " " + 7);
+console.log(message + " " + 8);
+console.log(message + " " + 9);
